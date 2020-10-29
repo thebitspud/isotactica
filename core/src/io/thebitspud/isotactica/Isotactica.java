5c7c5e2d6445340ca69e8afe992bbcee4747e1ae
James Zhong <thebitspud@gmail.com>
2020-10-29 00:04:33 -0700

    Added basic screens

--- core/src/io/thebitspud/isotactica/Isotactica.java
+++ core/src/io/thebitspud/isotactica/Isotactica.java
@@ -1,23 +1,65 @@
 package io.thebitspud.isotactica;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;\
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import io.thebitspud.isotactica.screens.GameScreen;
+import io.thebitspud.isotactica.screens.PauseScreen;
+import io.thebitspud.isotactica.screens.TitleScreen;
+
+import java.util.HashMap;
+
+/**
+ * The Game class from which all other classes emerge
+ * Global variables and common functions go here
+ */
 
 public class Isotactica extends Game {
-	SpriteBatch batch;
+	private SpriteBatch batch;
+
+	public enum ScreenID {
+		TITLE,
+		GAME,
+		PAUSE
+	}
+
+	private HashMap<ScreenID, Screen> screens;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+
+		// finish asset loading here
+
+		screens = new HashMap<>();
+		screens.put(ScreenID.TITLE, new TitleScreen());
+		screens.put(ScreenID.GAME, new GameScreen());
+		screens.put(ScreenID.PAUSE, new PauseScreen());
+
+		setScreen(getScreen(ScreenID.TITLE));
 	}
 
 	@Override
 	public void render () {
-
+		super.render();
 	}
 	
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
+	}
+
+	/* Getters and Setters */
+
+	public Screen getScreen(ScreenID id) {
+		return screens.get(id);
+	}
+
+	public void setScreen(ScreenID id) {
+		setScreen(screens.get(id));
+	}
+
+	public SpriteBatch getBatch() {
+		return batch;
 	}
 }
